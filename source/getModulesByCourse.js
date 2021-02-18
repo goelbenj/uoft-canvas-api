143c87e929f7afd19bfaea828d819e52af926219
goelbenj <goelbenj@ecf.utoronto.ca>
2021-02-17 21:19:35 -0500

    Add getModulesByCourse function

---
+++ source/getModulesByCourse.js
@@ -0,0 +1,16 @@
+import fetch from './internal/fetch';
+
+require('dotenv').config();
+
+const canvasDomain = process.env.CANVAS_API_DOMAIN;
+
+/**
+ * Get a paginated list of modules in a course by courseId
+ * @param {Number} courseId the course id.
+ * @return {Promise} A promise that resolves to a list of Module objects: https://canvas.instructure.com/doc/api/modules.html#method.context_modules_api.index
+ */
+
+export default function getModulesByCourse (courseId) {
+    return fetch(canvasDomain + `/courses/${courseId}/modules`);
+};
+
